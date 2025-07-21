e8365ec273b246b491cd61479f2e0b206ba87735
Eduardo Marçal <edumsfilho347@gmail.com>
2025-07-21 16:12:44 -0300

    fix: producer serializer and deserializer options

--- api/src/main/kotlin/com/detector/api/kafka/producer/KafkaProducerConfig.kt
+++ api/src/main/kotlin/com/detector/api/kafka/producer/KafkaProducerConfig.kt
@@ -1,24 +1,28 @@
 package com.detector.api.kafka.producer
 
-import org.apache.kafka.clients.producer.ProducerConfig
 
-import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
-class KafkaProducerConfig {
+class KafkaProducerConfig (
+    @Value($$"${kafka.bootstrap-server}")
+    val bootstrapServer: String
+){
 
     @Bean
     fun kafkaProducerFactory(): ProducerFactory<String?, Any?> {
         val properties = mutableMapOf<String?, Any?>()
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer::class.java.name)
         properties.put(ProducerConfig.LINGER_MS_CONFIG, "20")
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG, Integer.valueOf(32 * 1024).toString())
         properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy")
