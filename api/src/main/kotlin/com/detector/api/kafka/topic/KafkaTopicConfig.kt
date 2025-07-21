35fa267828d999c8603ace51d239ea9c1827eb52
Eduardo Marçal <edumsfilho347@gmail.com>
2025-07-21 16:13:37 -0300

    fix: using compact only in threshold topic and removing unused topic

--- api/src/main/kotlin/com/detector/api/kafka/topic/KafkaTopicConfig.kt
+++ api/src/main/kotlin/com/detector/api/kafka/topic/KafkaTopicConfig.kt
@@ -1,39 +1,40 @@
 package com.detector.api.kafka.topic
 
 import org.apache.kafka.clients.admin.AdminClientConfig
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.KafkaAdmin
 
 @Configuration
-class KafkaTopicConfig {
+class KafkaTopicConfig(
+    @Value("\${kafka.bootstrap-server}")
+    val bootstrapServer: String,
+
+    @Value( "\${kafka.topics.transaction}" )
+    val transactionTopic: String,
+
+    @Value( "\${kafka.topics.threshold}" )
+    val thresholdTopic: String
+
+) {
 
     @Bean
-    fun admin() = KafkaAdmin(mapOf(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092"))
+    fun admin() = KafkaAdmin(mapOf(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServer))
 
     @Bean
     fun transactionTopic() =
-        TopicBuilder.name("user.transaction.queue")
+        TopicBuilder.name(transactionTopic)
             .partitions(3)
             .replicas(2)
-            .compact()
             .build()
 
     @Bean
     fun transactionThreshouldTopic() =
-        TopicBuilder.name("user.transaction.threshold")
+        TopicBuilder.name(thresholdTopic)
             .partitions(3)
             .replicas(2)
             .compact()
             .build()
-
-    @Bean
-    fun suspiciousTransactionTopic() =
-        TopicBuilder.name("user.transaction.suspicious")
-            .partitions(3)
-            .replicas(2)
-            .compact()
-            .build()
-
 }
