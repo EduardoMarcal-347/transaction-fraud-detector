e7a511c7f3e2217b9374e313e2fe965ed3a16afd
Eduardo Marçal <edumsfilho347@gmail.com>
2025-07-23 10:12:34 -0300

    feat: update transactions by processor response

--- api/src/main/kotlin/com/detector/api/services/FinancialTransactionService.kt
+++ api/src/main/kotlin/com/detector/api/services/FinancialTransactionService.kt
@@ -1,6 +1,7 @@
 package com.detector.api.services
 
 import com.detector.api.dto.mapper.FinancialTransactionMapper
+import com.detector.api.dto.message.ProcessedTxDTO
 import com.detector.api.dto.request.FinancialTransactionReq
 import com.detector.api.entities.FinancialTransaction
 import com.detector.api.enums.TransactionStatus
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 import java.util.UUID
+import java.time.Instant
 
 @Service
 class FinancialTransactionService(
@@ -21,11 +23,11 @@ class FinancialTransactionService(
 
     fun createTransaction(req: FinancialTransactionReq): FinancialTransaction {
         val transaction = FinancialTransactionMapper.INSTANCE.toBean(req)
-        userService.validateUserExists(transaction.payer.id!!, "Payer")
-        userService.validateUserExists(transaction.receiver.id!!, "Receiver")
+        userService.validateUserExists(transaction.payer!!.id!!, "Payer")
+        userService.validateUserExists(transaction.receiver!!.id!!, "Receiver")
 
         val entity = transactionRepository.save(transaction)
-        kafkaTemplate.send( transactionTopic, entity.payer.id.toString(), entity)
+        kafkaTemplate.send( transactionTopic, entity.payer!!.id.toString(), entity)
         return entity
     }
 
@@ -37,8 +39,16 @@ class FinancialTransactionService(
         return transactionRepository.findAllByPayerIdAndStatus(payerId, status)
     }
 
-    fun findTransaction(transactionId: Int) {
-        transactionRepository.findById(transactionId)
+    fun findTransaction(transactionId: Int) : FinancialTransaction{
+        return transactionRepository.findById(transactionId)
+            .orElseThrow { NoSuchElementException("Unknown Transaction with id $transactionId") }
+    }
+
+    fun updateProcessedTransaction(processedTx: ProcessedTxDTO) {
+        val tx = findTransaction(processedTx.id)
+        tx.status = processedTx.status
+        tx.processedAt = Instant.now()
+        transactionRepository.save(tx)
     }
 
 }
