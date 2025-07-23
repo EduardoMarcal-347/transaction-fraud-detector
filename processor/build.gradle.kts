fab5ad75458e12a9f10c0b50958a7d008a54543b
Eduardo Marçal <edumsfilho347@gmail.com>
2025-07-23 10:15:00 -0300

    chore: adding processor build files

---
+++ processor/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.detector"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.kafka:kafka-streams")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.15.3")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.3")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
